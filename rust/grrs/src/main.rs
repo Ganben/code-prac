0eda015f18a03efa96208e34b4dc3a1eabf99d31
Ganben <caozhao@gmail.com>
2019-09-16 13:02:45 +0800

    rust io /py transformer bump

--- rust/grrs/src/main.rs
+++ rust/grrs/src/main.rs
@@ -1,5 +1,8 @@
 use structopt::StructOpt;
-
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+// use std::io::{self, BufRead};
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 
@@ -10,9 +13,33 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     // println!("Hello, world!");
     // let pattern = std::env::args().nth(1).expect("no pattern given");
     // let path = std::env::args().nth(2).expect("no path given");
     let args = Cli::from_args();
+    // let content = std::fs::read_to_string(&args.path)
+    let f = File::open(&args.path)?;
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+    
+    let content = reader.read_line(&mut line)?;
+    //   .expect("reading file faile");
+    while content > 0 {
+        
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+
+        line.clear();
+        let content = reader.read_line(&mut line)?;
+        
+    }
+    line.clear();
+    // for line in content {
+    //     if line.contains(&args.pattern) {
+    //         println!("{}", line);
+    //     }
+    // }
+    Ok(())
 }

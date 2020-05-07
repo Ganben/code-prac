e878daf112b049788ab3c80bec6caba9d9876625
Ganben <caozhao@gmail.com>
2020-05-07 18:45:43 +0800

    bump daily

---
+++ go/LeetCode1/SubtreeAnother/solution_test.go
@@ -0,0 +1,32 @@
+package SubtreeAnother
+
+import "testing"
+
+var cases = []struct {
+	name   string
+	input1 []int
+	input2 []int
+	expect bool
+}{
+	{
+		"test 1",
+		[]int{3, 4, 5, 1, 2, -1, -1},
+		[]int{4, 1, 2},
+		true,
+	},
+}
+
+func generateTree(arr []int) *TreeNode {
+
+}
+
+func TestSolution(t *testing.T) {
+	for _, c := range cases {
+		r1 := generateTree(c.input1)
+		r2 := generateTree(c.input2)
+		got := isSubtree(r1, r2)
+		if got != c.expect {
+			t.Fatalf("%s incorrect by %v", c.name, got)
+		}
+	}
+}

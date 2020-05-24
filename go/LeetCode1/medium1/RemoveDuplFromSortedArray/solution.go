a50fde89cc60619a5066192f28e176c56484dfc4
Ganben <caozhao@gmail.com>
2020-05-24 22:55:48 +0800

    bump wordsearch full

--- go/LeetCode1/medium1/RemoveDuplFromSortedArray/solution.go
+++ go/LeetCode1/medium1/RemoveDuplFromSortedArray/solution.go
@@ -2,22 +2,21 @@ package RemoveDuplFromSortedArray
 
 func removeDuplicates(nums []int) int {
 	i, count := 1, 1
-	for i < len(nums) {
+	for ; i < len(nums); i++ {
 		if nums[i] == nums[i-1] {
 			count++
 			if count > 2 {
 				tmp1 := nums[:i]
 				tmp2 := nums[i+1:]
-				nums = []int{}
-				nums = append(nums, tmp1...)
-				nums = append(nums, tmp2...)
-
+				tmp := []int{}
+				tmp = append(nums, tmp1...)
+				tmp = append(nums, tmp2...)
+				nums = tmp
 			}
 
 		} else {
 			count = 1
 		}
-		i++
 	}
 	return len(nums)
 }
--- go/LeetCode1/medium1/WordSearch/solution.go
+++ go/LeetCode1/medium1/WordSearch/solution.go
@@ -3,9 +3,11 @@ package search
 var visited [][]bool
 var last []int
 var ans bool
+var directions [][]int
 
 func exist(board [][]byte, word string) bool {
 	ans = false
+	directions = [][]int{[]int{0, 1}, []int{1, 0}, []int{0, -1}, []int{-1, 0}}
 	for _, row := range board {
 		tmp := make([]bool, len(row))
 		visited = append(visited, tmp)
@@ -29,6 +31,17 @@ func search(board [][]byte, i int, j int, word string) {
 		return
 	}
 	// case 4 directions
-	locs := [][]int{}
+	for _, c := range directions {
+		ii := i + c[0]
+		jj := j + c[1]
+		if ii >= 0 && ii < len(board) && jj >= 0 && jj < len(board[0]) {
+			if board[ii][jj] == word[0] {
+				visited[ii][jj] = true
+				search(board, ii, jj, word[1:])
+				visited[ii][jj] = false
+			}
+		}
+
+	}
 
 }

4dbe15c3423ebe61508004d70e25a54c43a9da39
Ganben <caozhao@gmail.com>
2020-05-31 23:31:06 +0800

    bump 84

---
+++ go/LeetCode1/Hards/maximalRect/solution.go
@@ -0,0 +1,45 @@
+package solution
+
+func maximalRectangle(matrix [][]byte) int {
+	if matrix == nil {
+		return 0
+	}
+	maxarea := 0
+	dp := make([]int, len(matrix[0]))
+	for _, row := range matrix {
+		for j, nn := range row {
+			if nn == '1' {
+				dp[j] = dp[j] + 1
+			} else {
+				dp[j] = 0
+			}
+
+		}
+		maxarea = max(maxarea, stackHelper(dp))
+	}
+	return maxarea
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func stackHelper(heights []int) int {
+	stack := []int{-1}
+	maxarea := 0
+	for i, n := range heights {
+		for stack[len(stack)-1] != -1 && heights[stack[len(stack)-1]] >= n {
+			maxarea = max(maxarea, heights[stack[len(stack)-1]]*(len(heights)-stack[len(stack)-2]-1))
+			stack = stack[:len(stack)]
+		}
+		stack = append(stack, i)
+	}
+	for stack[len(stack)-1] != -1 {
+		maxarea = max(maxarea, heights[stack[len(stack)-1]]*(len(heights)-stack[len(stack)-2]-1))
+		stack = stack[:len(stack)]
+	}
+	return maxarea
+}

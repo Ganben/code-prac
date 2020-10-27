5f989474e005ef5b42045db40541d6c295cb2c8b
Ganben <caozhao@gmail.com>
2020-10-27 22:06:17 +0800

    b299

---
+++ go/LeetCode1/0Medium/BullsAndCows/solution.go
@@ -0,0 +1,28 @@
+package BullsAndCows
+
+import "strconv"
+
+func getHint(secret string, guess string) string {
+	a, b := 0, 0
+	mapS, mapG := make([]int, 10), make([]int, 10)
+	for i := range secret {
+		tmp, charGuess := secret[i], guess[i]
+		if tmp == guess[i] {
+			a++
+		} else {
+			mapS[tmp-'0']++
+			mapG[charGuess-'0']++
+		}
+	}
+	for i := 0; i < 10; i++ {
+		b += min(mapS[i], mapG[i])
+	}
+	return strconv.Itoa(a) + "A" + strconv.Itoa(b) + "B"
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}

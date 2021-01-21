d51f7f3156b3bc03ec5d78570dc125a2fb88c586
Ganben <caozhao@gmail.com>
2021-01-21 22:46:15 +0800

    bump 373

---
+++ go/LeetCode1/0Easy/GuessNumber/main.go
@@ -0,0 +1,22 @@
+package main
+
+func guessNumber(n int) int {
+	low := 1
+	high := n
+	for low <= high {
+		mid := low + (high-low)/2
+		res := guess(mid)
+		if res == 0 {
+			return mid
+		} else if res < 0 {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+
+}

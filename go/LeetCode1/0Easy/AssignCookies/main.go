484ba032eb3130e5d0e1a20d988cc42afd200c4c
Ganben <caozhao@gmail.com>
2021-05-26 23:32:23 +0800

    bump 455

--- go/LeetCode1/0Easy/AssignCookies/main.go
+++ go/LeetCode1/0Easy/AssignCookies/main.go
@@ -1,7 +1,22 @@
 package main
 
-func findContentChildren(g []int, s []int) int {
+import "sort"
 
+func findContentChildren(g []int, s []int) int {
+	sort.Ints(g)
+	sort.Ints(s)
+	ans := 0
+	n, m := len(g), len(s)
+	for i, j := 0, 0; i < n && j < m; i++ {
+		for j < m && g[i] > s[j] {
+			j++
+		}
+		if j < m {
+			ans++
+			j++
+		}
+	}
+	return ans
 }
 
 func main() {

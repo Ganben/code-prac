1cb25435ec616c838984ebcb23e8421764c0d7f1
Ganben <caozhao@gmail.com>
2021-06-24 17:06:01 +0800

    bump 149

---
+++ go/LeetCode1/1Hard/MaxPointsOnLine/main.go
@@ -0,0 +1,60 @@
+package main
+
+func maxPoints(points [][]int) int {
+	n := len(points)
+	var ans int
+	if n <= 2 {
+		return n
+	}
+	for i, p := range points {
+		if ans >= n-i || ans > n/2 {
+			break
+		}
+		cnt := map[int]int{}
+		for _, q := range points[i+1:] {
+			x, y := p[0]-q[0], p[1]-q[1]
+			if x == 0 {
+				y = 1
+			} else if y == 0 {
+				x = 1
+			} else {
+				if y < 0 {
+					x, y = -x, -y
+				}
+				g := gcd(abs(x), abs(y))
+				x /= g
+				y /= g
+			}
+			cnt[y+x*20001]++
+		}
+		for _, c := range cnt {
+			ans = max(ans, c+1)
+		}
+	}
+	return ans
+}
+
+func gcd(a, b int) int {
+	for a != 0 {
+		a, b = b%a, a
+	}
+	return b
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+
+}

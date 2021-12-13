53d03b921a00b7af4c7a7377374980cafc378ff9
Ganben <caozhao@gmail.com>
2021-12-13 20:53:42 +0800

    bump 807

---
+++ go/LeetCode1/Medium3/MaxIncKeepCitySkyline/solve.go
@@ -0,0 +1,34 @@
+package maxinckeepcityskyline
+
+func maxIncreaseKeepingSkyline(grid [][]int) int {
+	ans := 0
+	n := len(grid)
+	rowMax := make([]int, n)
+	colMax := make([]int, n)
+	for i, row := range grid {
+		for j, h := range row {
+			rowMax[i] = max(rowMax[i], h)
+			colMax[j] = max(colMax[j], h)
+		}
+	}
+	for i, row := range grid {
+		for j, h := range row {
+			ans += min(rowMax[i], colMax[j]) - h
+		}
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
